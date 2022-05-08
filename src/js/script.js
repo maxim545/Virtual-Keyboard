54351aae64b53185ab159f8fa9761ac233e05076
maxim545 <maxximc95@gmail.com>
2022-05-08 15:11:08 +0300

    feat: implement switch keyboard between two languages

---
+++ src/js/script.js
@@ -0,0 +1,168 @@
+import '../style.css';
+
+import en from './export/en';
+import ru from './export/ru';
+import Init from './init';
+
+class ChangeValues {
+  constructor() {
+    this.obj = { eng: en, rus: ru };
+    this.lang = localStorage.getItem('lang');
+    this.capsLock = false;
+    this.shift = false;
+    this.shiftAndCaps = true;
+    this.keyPressed = {};
+    this.keysPress = {};
+    this.text = '';
+    this.curentKey = null;
+    this.eventType = null;
+    this.indexKey = null;
+    this.code = null;
+    this.count = 0;
+    this.repeat = false;
+    this.isKey = false;
+    this.typeIsDown = false;
+    this.mouseDownKeyIs = null;
+    this.isAlt = null;
+  }
+
+  catchEvent() {
+    document.addEventListener('keydown', (e) => { this.detecteKey(e); });
+    document.addEventListener('keyup', (e) => { this.detecteKey(e); });
+    document.addEventListener('mousedown', (e) => { this.detecteKey(e); });
+    document.addEventListener('mouseup', (e) => { this.detecteKey(e); });
+  }
+
+  detecteKey(e) {
+    const existKeys = [];
+    let allKeys = document.querySelectorAll('.keyboard__key');
+    allKeys = [...allKeys];
+    allKeys.forEach((key, i) => {
+      if (key.dataset.code === e.code || key.dataset.code === e.target.dataset.code || e.type === 'mouseup') {
+        if (e.type === 'keydown' || e.type === 'keyup') {
+          this.curentKey = key;
+          this.eventType = e.type;
+          this.code = e.code;
+          this.indexKey = i;
+          this.repeat = e.repeat;
+          this.isKey = true;
+          if (e.type === 'keydown') {
+            this.typeIsDown = e.code;
+            this.keysPress[e.code] = true;
+          }
+          if (e.type === 'keyup') {
+            this.typeIsDown = false;
+            this.keysPress[e.code] = false;
+          }
+        }
+        if ((e.type === 'mousedown' || e.type === 'mouseup') && (this.typeIsDown !== e.target.dataset.code) && (!this.keysPress[e.target.dataset.code])) {
+          if (e.type === 'mousedown') {
+            this.mouseDownKeyIs = key;
+            this.eventType = 'keydown';
+            this.curentKey = key;
+            this.code = e.target.dataset.code;
+            this.indexKey = i;
+            this.isKey = true;
+          }
+          if (e.type === 'mouseup' && this.isKey) {
+            this.eventType = 'keyup';
+          }
+        }
+      }
+    });
+    if (e.type === 'mouseup' && this.isKey && e.target.dataset.code !== this.curentKey && this.mouseDownKeyIs) {
+      this.mouseDownKeyIs.classList.remove('active');
+    }
+    if (e.type === 'mousedown') {
+      this.isAlt = e.target.dataset.code;
+    }
+    if (e.type === 'mouseup' && !e.target.dataset.code) {
+      this.keyPressed[this.isAlt] = false;
+    }
+    this.obj[this.lang].forEach((key) => existKeys.push(key.code));
+    if ((this.eventType === 'keydown' || this.eventType === 'keyup') && this.isKey && existKeys.includes(e.code || e.target.dataset.code)) {
+      if (((this.code === 'ControlLeft' || this.code === 'AltLeft') && !this.repeat) || (e.type === 'mouseup' || e.type === 'mousedown')) {
+        this.changeLang(e);
+      }
+      this.switchKeys();
+
+      if (e.type === 'mouseup') { this.isKey = false; }
+    }
+  }
+
+  changeLang(e) {
+    if ((this.eventType === 'keydown' || e.type === 'mousedown') && (this.code === 'ControlLeft' || this.code === 'AltLeft')) {
+      this.keyPressed[this.code] = true;
+      if (this.keyPressed.ControlLeft && this.keyPressed.AltLeft) {
+        if (localStorage.getItem('lang') === 'eng') {
+          localStorage.setItem('lang', 'rus');
+        } else { localStorage.setItem('lang', 'eng'); }
+        this.lang = localStorage.getItem('lang');
+        this.create();
+      }
+    }
+    if (e.type === 'keyup') { this.keyPressed[this.code] = false; }
+    if (e.type === 'mouseup') { this.keyPressed[e.target.dataset.code] = false; }
+  }
+
+  switchKeys() {
+    if (this.eventType === 'keydown' && this.code.match(/ShiftLeft|ShiftRight/g) && !this.shift) {
+      if (!this.repeat) {
+        this.shift = !this.shift;
+        this.capsLock = !this.capsLock;
+        this.shiftAndCaps = !this.shiftAndCaps;
+        this.create();
+      }
+    } else if (this.eventType === 'keydown' && this.code === 'CapsLock') {
+      if (!this.repeat) {
+        this.capsLock = !this.capsLock;
+        this.create();
+      }
+    }
+    if (this.eventType === 'keyup' && this.code.match(/ShiftLeft|ShiftRight/g) && this.shift) {
+      if (!this.repeat) {
+        this.shift = !this.shift;
+        this.capsLock = !this.capsLock;
+        this.shiftAndCaps = !this.shiftAndCaps;
+        this.create();
+      }
+    }
+  }
+
+  create() {
+    const key = new Init(this.capsLock, this.shift, this.shiftAndCaps);
+    key.creatLetters();
+  }
+
+  refresh() {
+    this.capsLock = false;
+    this.shift = false;
+    this.shiftAndCaps = true;
+    this.keyPressed = {};
+    this.text = '';
+    this.curentKey = null;
+    this.eventType = null;
+    this.indexKey = null;
+    this.code = null;
+    this.repeat = false;
+    this.isKey = false;
+    this.typeIsDown = false;
+    this.mouseDownKeyIs = null;
+    this.create();
+    let allKeys = document.querySelectorAll('.keyboard__key');
+    allKeys = [...allKeys];
+    allKeys.forEach((key) => {
+      if (key.classList.contains('active_capslock')) {
+        key.classList.remove('active_capslock');
+      }
+      if (key.classList.contains('active')) {
+        key.classList.remove('active');
+      }
+    });
+  }
+}
+const value = new ChangeValues();
+value.catchEvent();
+
+function disable() { document.onkeydown = () => false; } disable();
+window.addEventListener('blur', () => { value.refresh(); });
